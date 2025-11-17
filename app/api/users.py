55062880873153e7ffbcdac8cd2eff2223e2a8d7
YenJack <jack@yenprotek.com>
2025-11-17 11:57:27 +0800

    Update users.py

--- app/api/users.py
+++ app/api/users.py
@@ -1,31 +1,22 @@
-
-# app/api/users.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-
-from app.core.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserRead
+from app.core.database import get_db
 
-#router = APIRouter()
-router = APIRouter(
-    prefix="/users",
-    tags=["users"]
-)
+router = APIRouter()
 
 @router.post("/", response_model=UserRead)
 async def create_user(payload: UserCreate, db: AsyncSession = Depends(get_db)):
     user = User(name=payload.name, email=payload.email)
     db.add(user)
-
     try:
         await db.commit()
         await db.refresh(user)
     except:
         await db.rollback()
         raise HTTPException(status_code=400, detail="Email already exists")
-
     return user
 
 @router.get("/", response_model=list[UserRead])
