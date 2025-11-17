81db7390acf655661f201fb990cce8189b36b1f0
YenJack <jack@yenprotek.com>
2025-11-17 14:38:23 +0800

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,14 +1,19 @@
 from pydantic import BaseModel
 
 class UserCreate(BaseModel):
-    name: str
-    email: str
+    username: str
+    email_address: str
+    password: str
 
 class UserRead(BaseModel):
     id: int
-    name: str
-    email: str
+    username: str
+    email_address: str
+    full_name: str | None = None
 
     class Config:
         orm_mode = True
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
