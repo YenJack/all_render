12d9d137290bc9c37efb629a3dd39eb40cb32510
YenJack <jack@yenprotek.com>
2025-11-17 11:57:48 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# app/main.py
 from fastapi import FastAPI
 from app.api.users import router as users_router
 
@@ -7,5 +6,5 @@ app = FastAPI(title="FastAPI + PostgreSQL + Render")
 app.include_router(users_router, prefix="/users")
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "FastAPI Running"}
