54cd8ade44e636afc80d93bf53fb79d144202327
YenJack <jack@yenprotek.com>
2025-11-17 11:12:19 +0800

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+# app/models/user.py
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    email: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)
