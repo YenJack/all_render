b34d394324424d5303f7210e6af76d2e9c76062f
YenJack <jack@yenprotek.com>
2025-11-17 16:17:57 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,18 @@
 from fastapi import FastAPI
-#from app.api import auth, users, farms, devices, sensors
-#from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 from app.api.farms import router as farms_router
 from app.api.devices import router as devices_router
 from app.api.sensors import router as sensors_router
 
-app = FastAPI()
-
-app.include_router(auth_router)
-app.include_router(users_router)
-app.include_router(farms_router)
-app.include_router(devices_router)
-app.include_router(sensors_router)
-
-
 app = FastAPI(title="Farm FastAPI PostgreSQL + JWT")
 
-app.include_router(auth.router, prefix="/auth")
-app.include_router(users.router, prefix="/users")
-app.include_router(farms.router, prefix="/farms")
-app.include_router(devices.router, prefix="/devices")
-app.include_router(sensors.router, prefix="/sensors")
+# 🔥 一定要加 prefix 才會變成 /auth/register
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(users_router, prefix="/users", tags=["Users"])
+app.include_router(farms_router, prefix="/farms", tags=["Farms"])
+app.include_router(devices_router, prefix="/devices", tags=["Devices"])
+app.include_router(sensors_router, prefix="/sensors", tags=["Sensors"])
 
 @app.get("/")
 async def root():
