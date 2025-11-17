cbf556c656a6b9f124a26667c64bd89399a98a0f
YenJack <jack@yenprotek.com>
2025-11-17 11:12:54 +0800

    Create user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,14 @@
+# app/schemas/user.py
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+class UserRead(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
